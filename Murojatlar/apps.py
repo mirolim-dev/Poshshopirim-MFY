69da504eefeb66bf56258cd6720dd40bf6324a5e
mrdjangodev <mirolimcoder@gmail.com>
2023-08-11 18:28:44 +0500

    Murojatlar been created

---
+++ Murojatlar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MurojatlarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Murojatlar'
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
      # apps
     'main',
     'news',
+    'Murojatlar'
     
     # packages
     'ckeditor', 
