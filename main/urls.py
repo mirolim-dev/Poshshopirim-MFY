93f36eb8015afa82c00408d59a8c77268e0f5730
mrdjangodev <mirolimcoder@gmail.com>
2023-08-14 12:31:02 +0500

    workers section done

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home
+from .views import home, workers_view
 from news.views import post
 
 urlpatterns = [
     path('', post, name='home'),
+    path('workers/', workers_view, name='workers_view'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Worker
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+
+def workers_view(request):
+    workers = Worker.objects.filter(is_active=True)
+    context = {
+        'workers': workers,
+        }
+    return render(request, 'about.html', context)
