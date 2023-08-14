737b2a94445f50fc5e2c4cfab7d6a57bbf92f8e7
mrdjangodev <mirolimcoder@gmail.com>
2023-08-14 22:40:09 +0500

    counting by ip address done

--- news/views.py
+++ news/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+# from django.contrib.gis.geoip2 import GeoIP2
+from ipware import get_client_ip
 
-from .models import Post, PostCategory
+from .models import Post, PostCategory, ViewedIP
 # Create your views here.
 
 def post(request):
@@ -30,12 +33,26 @@ def posts_by_category(request, tag):
     return render(request, 'blog.html', context)
 
 
+def is_ip_address_counted(post, ip_address):
+    """It is belong to post detail"""
+    return post.viewedip_set.filter(ip_address=ip_address).exists()
+
 def post_detail(request, pk:int):
+    post = get_object_or_404(Post, id=pk)
+    client_ip, is_routable = get_client_ip(request)
+
+    
+    print(f"Client IP: {client_ip} | is routable: {is_routable}")
+    if client_ip is not None:
+        # Check if the IP address has already been counted for this post
+        if not is_ip_address_counted(post, client_ip):
+            # Increment the views count and store the IP address
+            ViewedIP.objects.create(post=post, ip_address=client_ip)
     posts = Post.objects.prefetch_related('author')
     categories = PostCategory.objects.all()
     recent_posts = posts if posts.count() < 6 else posts[-6:]
     context = {
-        'post': Post.objects.get(id=pk),
+        'post': post,
         'categories': categories,
         'recent_posts': recent_posts,
     }
