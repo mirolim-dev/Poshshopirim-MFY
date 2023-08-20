090468f5083d6ee077ff2a504893767454aca8ed
mrdjangodev <mirolimcoder@gmail.com>
2023-08-20 23:08:58 +0500

    extra models been hidden from admin dashboard

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
 from .models import Worker
 # Register your models here.
 
@@ -8,3 +10,5 @@ class WorkerAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name', 'phone']
 
 admin.site.register(Worker, WorkerAdmin)
+admin.site.unregister(User)
+admin.site.unregister(Group)
